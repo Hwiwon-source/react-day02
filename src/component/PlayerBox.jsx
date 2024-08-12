25651fa7165c0c8c39cb242237c3b65a87cad8b3
Hwiwon Kang <syaron9364@gmail.com>
2024-08-13 03:13:42 +0900

    second commit

--- src/component/PlayerBox.jsx
+++ src/component/PlayerBox.jsx
@@ -8,7 +8,7 @@ function PlayerBox(props) {
       <h2>YOU</h2>
       <div className='img-wrap'>
         {/** 첫 UI 렌더링 시, null이 리턴될 경우 가드 역할 */}
-        <img src={props.item && props.item.img} />
+        <img src={props.item && props.item.img} alt="playerChoice" />
       </div>
     </div>
   )
