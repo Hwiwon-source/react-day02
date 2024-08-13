1370d277eccfc50ba022a5ef8d8f1ae41179e5f2
Hwiwon Kang <syaron9364@gmail.com>
2024-08-13 16:18:35 +0900

    Add defaultImg

--- src/component/PlayerBox.jsx
+++ src/component/PlayerBox.jsx
@@ -3,12 +3,13 @@ import "./PlayerBox.css"
 
 function PlayerBox(props) {
   console.log(props)
+
   return (
     <div className='PlayerBox'>
       <h2>{props.name}</h2>
       <div className='img-wrap'>
         {/** 첫 UI 렌더링 시, null이 리턴될 경우 가드 역할 */}
-        <img src={props.item && props.item.img} alt="playerChoice" />
+        <img src={props.item ? props.item.img : "https://play-lh.googleusercontent.com/0wrJrG6Baz22NfndkMCw8ESisWC57BMmtEK7ZOEuaqkpCQuPPS7v_BBLRLj1uGdKfR5Q"} alt="playerChoice" />
       </div>
     </div>
   )
