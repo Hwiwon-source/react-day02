6edf46f6753d00cf9105797b7b0500c575cb84d0
Hwiwon Kang <syaron9364@gmail.com>
2024-08-14 00:30:01 +0900

    4th commit

--- src/component/PlayerBox.jsx
+++ src/component/PlayerBox.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import "./PlayerBox.css"
+import React from "react";
+import "./PlayerBox.css";
 
 function PlayerBox(props) {
-  // console.log(props)
+  const winCheck = () => {
+    if (props.result !== "" && props.result === "Draw!") {
+      return "draw";
+    } else if (props.result != "" && props.result === "Win!") {
+      return "win";
+    } else if (props.result != "" && props.result === "Lose :<") {
+      return "lose";
+    }
+  };
 
   return (
-    <div className='PlayerBox'>
+    <div className={`PlayerBox ${winCheck()}`}>
       <h2>{props.name}</h2>
-      <div className='img-wrap'>
+      <div className="img-wrap">
         {/** 첫 UI 렌더링 시, null이 리턴될 경우 가드 역할 */}
-        <img src={props.item ? props.item.img : "https://github.com/Hwiwon-source/react-day02/raw/main/public/defuat_img.png"} alt="playerChoice" />
+        <img
+          src={
+            props.item
+              ? props.item.img
+              : "https://github.com/Hwiwon-source/react-day02/raw/main/public/defuat_img.png"
+          }
+          alt="playerChoice"
+        />
       </div>
+      <p>{props.result}</p>
     </div>
-  )
+  );
 }
 
-export default PlayerBox
+export default PlayerBox;
